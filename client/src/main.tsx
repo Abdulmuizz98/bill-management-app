95a23029bec216ef67757ce4ca80ba1537d4b09c
Abdulmuizz98 <hamzatabdulmuizz98@gmail.com>
2024-07-23 18:30:52 +0100

    feat: second submission

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,7 +9,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-      <App />
     </Provider>
   </React.StrictMode>
 );
