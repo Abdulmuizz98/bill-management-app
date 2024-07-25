9582d60e7c4b2d6d9bdf1f8953dd69a0b81918c5
Abdulmuizz98 <hamzatabdulmuizz98@gmail.com>
2024-07-25 10:45:47 +0100

    fix: client irreg

--- client/src/components/ProviderRadio.tsx
+++ client/src/components/ProviderRadio.tsx
@@ -12,12 +12,9 @@ export const ProviderRadio = ({ field, onChange }: ProviderRadioProps) => {
   let choiceRef = useRef<HTMLLabelElement | null>(null);
 
   function handleSelection(e: React.MouseEvent<HTMLLabelElement, MouseEvent>) {
-    console.log(e.currentTarget);
     choiceRef.current = e.currentTarget; // Get the label
-    const radioBtn = choiceRef.current.querySelector(
-      "input[type=radio"
-    ) as HTMLInputElement;
 
+    console.log(choiceRef.current);
     // Find label with choice-provider class and toggle class off
     const prevLabel = providerRef.current?.querySelector(
       "label.choice-provider"
@@ -26,27 +23,26 @@ export const ProviderRadio = ({ field, onChange }: ProviderRadioProps) => {
 
     // toggle choice-provider class on for new choice
     choiceRef.current.classList.toggle("choice-provider");
-    radioBtn!.checked = true;
   }
 
   return (
     <div className="flex flex-row w-full gap-2" ref={providerRef}>
       {providers.map(({ name, value, icon }, index) => (
         <label
+          onClick={handleSelection}
           key={index}
           htmlFor={name}
           className="provider-label flex-1 cursor-pointer flex flex-col justify-center items-center space-y-[5px]"
-          onClick={handleSelection}
         >
           <div className="wrapper relative border-[0.5px] border-[#D0D5DD] rounded-[5px] w-full">
             <input
               type="radio"
-              id="provider"
+              id={name}
               name="provider"
               value={value}
               checked={field === value}
               onChange={onChange}
-              className="absolute"
+              className="absolute opacity-0 h-0 w-0 cursor-pointer"
             />
             <img src={icon} alt={name} className=" w-full " />
             <span className="icon absolute right-[3px] top-[3px]">
